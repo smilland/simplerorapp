b4278cc3f335bd0cacef3f00257eb1cefb099f19
Hang Li <lih69@lih69-1tp7xk.dhcp.gene.com>
2013-06-10 15:24:29 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '3015c492772157cdf9beefd06d2018326650c63f3bf625cf34aeba33b275c4ac1bd35480a3826e5423753baea517de5020ae6fdfedce60e9125d097e2affac6b'
